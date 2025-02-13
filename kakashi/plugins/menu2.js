86350190979015128531e384a2b56521931f1052
Malvin King <malvink003@gmail.com>
2025-02-13 15:24:07 +0200

    Update menu2.js

--- kakashi/plugins/menu2.js
+++ kakashi/plugins/menu2.js
@@ -63,7 +63,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/kingmalvn/malvin-database/raw/refs/heads/main/Media/menu.mp3' },
+            audio: { url: 'https://github.com/SoloLord201/Kakashi-database/raw/refs/heads/main/Media/menu2.mp3' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
